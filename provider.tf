c0dfc46b19e47aedc8bd1734d3e9cd54eb20036e
Shree <atish202@gmail.com>
2023-02-15 23:11:35 +0530

    vpc first commit

--- provider.tf
+++ provider.tf
@@ -1,3 +1,5 @@
- provider "aws" {
-   region = "us-east-1"
- }
+provider "aws" {
+  region = "us-east-1"
+  access_key = "AKIARNT2RNSHUSXRVOVN"
+  secret_key = "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"
+}
